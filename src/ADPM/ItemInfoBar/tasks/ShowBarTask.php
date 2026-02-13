e8be0c07d5647542e7a106ae822c211e48b09d63
EyNoah1171 <javasnippets.coding@gmail.com>
2026-02-13 09:18:08 +0100

    dada

---
+++ src/ADPM/ItemInfoBar/tasks/ShowBarTask.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * @org AusiDevelopmentPM
+ * @author EyNoah1171
+ * @year 2026
+ * @license Dont steal my Code.
+ */
+
+namespace ADPM\ItemInfoBar\tasks;
+
+use ADPM\ItemInfoBar\Loader;
+use pocketmine\player\Player;
+use pocketmine\scheduler\Task;
+
+class ShowBarTask extends Task
+{
+
+    private Player $player;
+
+    public function __construct(Player $player)
+    {
+        $this->player = $player;
+    }
+
+    public function onRun(): void
+    {
+        foreach (Loader::getInstance()->getServer()->getOnlinePlayers() as $player) {
+            if ($player->isOnline()) {
+                $message = Loader::config()->getNested("actionbar-message");
+                $pos = $player->getPosition();
+                $item = $player->getInventory()->getItemInHand();
+                $message = str_replace("&", "§", $message);
+                $message = str_replace("{x}", $pos->getX(), $message);
+                $message = str_replace("{y}", $pos->getY(), $message);
+                $message = str_replace("{z}", $pos->getZ(), $message);
+                $message = str_replace("{item_name}", $item->getName(), $message);
+                $message = str_replace("{item_id}", $item->getType(), $message);
+                $player->sendActionBarMessage($message);
+            }
+        }
+    }
+}
